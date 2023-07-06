f0a1625c56f1d746fdcd183d51b1f76d91a9b161
svet-nikol <plaksina.sn@mail.ru>
2023-07-06 12:24:24 +0300

    2.8 - 3, 4, 5

--- hw-js-8.js
+++ hw-js-8.js
@@ -1,5 +1,4 @@
 console.log(`Задание 1`);
-
 const people = [
     { name: 'Глеб', age: 29 },
     { name: 'Анна', age: 17 },
@@ -15,7 +14,6 @@ console.log(` `);
 
 
 console.log(`Задание 2`);
-
 function isPositive(number) {
     return number > 0 ;
 };
@@ -40,7 +38,48 @@ const peopleWGender = [
     {name: 'Анна', gender: 'female'},
     {name: 'Олег', gender: 'male'},
     {name: 'Оксана', gender: 'female'}
- ];
+];
  
- console.log(filter([3, -4, 1, 9], isPositive));
- console.log(filter(peopleWGender, isMale));
+console.log(filter([3, -4, 1, 9], isPositive));
+console.log(filter(peopleWGender, isMale));
+console.log(` `);
+
+
+
+// console.log(`Задание 3`);
+// let result = confirm(`запустить вывод текущей даты каждые 3 сек в течение 30 сек?`); 
+// if (result) {
+//     let interval = setInterval(() => {
+//         let currentTime = new Date();
+//         console.log(currentTime.toDateString() + ' ' + currentTime.toLocaleTimeString('ru-Ru'));
+//     }, 1000*3);
+//     setTimeout(() => {
+//         clearInterval(interval);
+//         console.log(`30 секунд прошло`);
+//     }, 1000*30);  
+// }   
+
+
+
+
+// console.log(`Задание 4`);
+// function delayForSecond(callback) {
+//     setTimeout(callback, 1000*1);
+// }
+// delayForSecond(function () {
+//   console.log('Привет, Глеб!');
+// })
+
+
+
+// console.log(`Задание 5`);
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+// 				if(cb) { 	cb(); }
+//     }, 1000)
+// }
+// function sayHi (name) {
+//     console.log(`Привет, ${name}!`);
+// }
+// delayForSecond(() => {sayHi('Глеб')})
